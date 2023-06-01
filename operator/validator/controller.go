ca015056c47f8ac44db43247340cc954c42ca71f
moshe-blox <moshe@blox.io>
2023-06-01 19:58:09 +0300

    disable ncv log

--- operator/validator/controller.go
+++ operator/validator/controller.go
@@ -374,11 +374,11 @@ func (c *controller) handleWorkerMessages(logger *zap.Logger, msg *spectypes.SSV
 				ncv,
 				time.Duration(ttlSlots)*c.beacon.GetBeaconNetwork().SlotDurationSec(),
 			)
-			logger.Debug("created non committee validator",
-				zap.String("id", msg.GetID().String()), zap.String("role", msg.MsgID.GetRoleType().String()),
-				zap.Duration("ttl", time.Duration(ttlSlots)*c.beacon.GetBeaconNetwork().SlotDurationSec()),
-				zap.Int("cache_size", c.nonCommitteeValidators.Len()),
-			)
+			// logger.Debug("created non committee validator",
+			// 	zap.String("id", msg.GetID().String()), zap.String("role", msg.MsgID.GetRoleType().String()),
+			// 	zap.Duration("ttl", time.Duration(ttlSlots)*c.beacon.GetBeaconNetwork().SlotDurationSec()),
+			// 	zap.Int("cache_size", c.nonCommitteeValidators.Len()),
+			// )
 		}
 
 		ncv.Lock()
